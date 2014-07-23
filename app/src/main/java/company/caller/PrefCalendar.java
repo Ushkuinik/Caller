ce9ad4b176da80b6a4441b9e5f9ea813920ac230
Balykin Nikolay <newest@mail.ru>
2014-07-23 10:24:51 +0900

    Added calendar selection

--- app/src/main/java/company/caller/PrefCalendar.java
+++ app/src/main/java/company/caller/PrefCalendar.java
@@ -1,32 +1,18 @@
 package company.caller;
 
-import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceGroup;
-import android.preference.PreferenceManager;
 import android.provider.CalendarContract;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -36,18 +22,15 @@ import java.util.concurrent.TimeUnit;
 public class PrefCalendar extends PreferenceActivity{
     final String LOG_TAG = this.getClass().toString();
     private EditText mEditTitle;
-    private PrefFragment m_PrefFragment;
+    private PreferenceFragmentNewEvent mPreferenceFragmentNewEvent;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        m_PrefFragment = new PrefFragment();
+        mPreferenceFragmentNewEvent = new PreferenceFragmentNewEvent();
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, m_PrefFragment).commit();
+                .replace(android.R.id.content, mPreferenceFragmentNewEvent).commit();
         setTitle(R.string.calendar_activity_title);
-
-//        mEditTitle = (EditText) findViewById(R.id.event_title);
     }
 
     @Override
@@ -83,11 +66,11 @@ public class PrefCalendar extends PreferenceActivity{
     private void saveCalendarEvent() {
         Log.d(this.LOG_TAG, "saveCalendarEvent");
 
-        String title = m_PrefFragment.findPreference("event_title").getSummary().toString();
-        String description = m_PrefFragment.findPreference("event_description").getSummary().toString();
+        String title = mPreferenceFragmentNewEvent.findPreference("event_title").getSummary().toString();
+        String description = mPreferenceFragmentNewEvent.findPreference("event_description").getSummary().toString();
 
-        long dateCurrentTZ = ((PreferenceDate)m_PrefFragment.findPreference("event_date")).getDate().getTimeInMillis();
-        long timeCurrentTZ = ((PreferenceTime)m_PrefFragment.findPreference("event_time")).getTime().getTimeInMillis();
+        long dateCurrentTZ = ((PreferenceDate) mPreferenceFragmentNewEvent.findPreference("event_date")).getDate().getTimeInMillis();
+        long timeCurrentTZ = ((PreferenceTime) mPreferenceFragmentNewEvent.findPreference("event_time")).getTime().getTimeInMillis();
 
         Calendar c = Calendar.getInstance();
         c.setTimeInMillis(dateCurrentTZ);
@@ -123,97 +106,5 @@ public class PrefCalendar extends PreferenceActivity{
 
 //        Toast.makeText(getApplicationContext(), "Save button was pressed! Title: " + s, Toast.LENGTH_SHORT).show();
 
-    }
-
-
-    /**
-     *
-     */
-    static public class PrefFragment extends PreferenceFragment
-            implements SharedPreferences.OnSharedPreferenceChangeListener {
-        final String LOG_TAG = this.getClass().toString();
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            Log.d(this.LOG_TAG, "onCreate");
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.advanced_preferences);
-            PreferenceManager.setDefaultValues(getActivity(), R.xml.advanced_preferences, false);
-
-            // populate Calendar list on creation
-            final ListPreference listCalendar = (ListPreference) findPreference("list_calendar");
-            setListPreferenceData(listCalendar);
-
-            initSummary(getPreferenceScreen());        }
-
-        @Override
-        public void onResume() {
-            Log.d(this.LOG_TAG, "onResume");
-            super.onResume();
-            getPreferenceScreen().getSharedPreferences().
-                    registerOnSharedPreferenceChangeListener(this);
-        }
-
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            Log.d(this.LOG_TAG, "onSharedPreferenceChanged");
-
-            updatePrefSummary(findPreference(key));
-        }
-
-        @Override
-        public void onPause() {
-            Log.d(this.LOG_TAG, "onPause");
-            super.onPause();
-            getPreferenceScreen().getSharedPreferences()
-                    .unregisterOnSharedPreferenceChangeListener(this);
-        }
-
-
-
-        /**
-         *
-         * @param _preference
-         */
-        private void initSummary(Preference _preference) {
-            Log.d(this.LOG_TAG, "initSummary");
-
-            if (_preference instanceof PreferenceGroup) {
-                PreferenceGroup group = (PreferenceGroup) _preference;
-                for (int i = 0; i < group.getPreferenceCount(); i++) {
-                    initSummary(group.getPreference(i));
-                }
-            }
-            else {
-                updatePrefSummary(_preference);
-            }
-        }
-
-
-
-        /**
-         *
-         * @param _preference
-         */
-        private void updatePrefSummary(Preference _preference) {
-            Log.d(this.LOG_TAG, "updatePrefSummary");
-
-            if (_preference instanceof ListPreference) {
-                ListPreference p = (ListPreference) _preference;
-                _preference.setSummary(p.getEntry());
-            }
-            else if (_preference instanceof EditTextPreference) {
-                EditTextPreference p = (EditTextPreference) _preference;
-                _preference.setSummary(p.getText());
-            }
-        }
-
-        protected static void setListPreferenceData(ListPreference _listPreference) {
-            CharSequence[] entries = { "Calendar1", "Calendar2" };
-            CharSequence[] entryValues = {"1" , "2"};
-            _listPreference.setEntries(entries);
-            _listPreference.setDefaultValue("1");
-            _listPreference.setEntryValues(entryValues);
-        }
     }
 }
---
+++ app/src/main/java/company/caller/PreferenceFragmentNewEvent.java
@@ -0,0 +1,191 @@
+package company.caller;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
+import android.provider.CalendarContract;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+public class PreferenceFragmentNewEvent extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    final String LOG_TAG = this.getClass().toString();
+
+    Context mContext = null;
+
+    public PreferenceFragmentNewEvent() {
+        Log.d(this.LOG_TAG, "PreferenceFragmentNewEvent");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(this.LOG_TAG, "onCreate");
+
+        addPreferencesFromResource(R.xml.advanced_preferences);
+        PreferenceManager.setDefaultValues(getActivity(), R.xml.advanced_preferences, false);
+
+        // populate Calendar list on creation
+        final ListPreference listCalendar = (ListPreference) findPreference("list_calendar");
+        setListPreferenceData(listCalendar);
+
+        initSummary(getPreferenceScreen());
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(this.LOG_TAG, "onResume");
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().
+                registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Log.d(this.LOG_TAG, "onAttach");
+        mContext = getActivity();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.d(this.LOG_TAG, "onSharedPreferenceChanged");
+
+        updatePrefSummary(findPreference(key));
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(this.LOG_TAG, "onPause");
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+
+
+    /**
+     *
+     * @param _preference
+     */
+    private void initSummary(Preference _preference) {
+        Log.d(this.LOG_TAG, "initSummary");
+
+        if (_preference instanceof PreferenceGroup) {
+            PreferenceGroup group = (PreferenceGroup) _preference;
+            for (int i = 0; i < group.getPreferenceCount(); i++) {
+                initSummary(group.getPreference(i));
+            }
+        }
+        else {
+            updatePrefSummary(_preference);
+        }
+    }
+
+
+
+    /**
+     *
+     * @param _preference
+     */
+    private void updatePrefSummary(Preference _preference) {
+        Log.d(this.LOG_TAG, "updatePrefSummary");
+
+        if (_preference instanceof ListPreference) {
+            ListPreference p = (ListPreference) _preference;
+            _preference.setSummary(p.getEntry());
+        }
+        else if (_preference instanceof EditTextPreference) {
+            EditTextPreference p = (EditTextPreference) _preference;
+            _preference.setSummary(p.getText());
+        }
+    }
+
+
+
+    /**
+     *
+     * @param _listPreference
+     */
+    protected void setListPreferenceData(ListPreference _listPreference) {
+
+        ArrayList<CharSequence> names = new ArrayList<CharSequence>();
+        ArrayList<CharSequence> values = new ArrayList<CharSequence>();
+
+        Map<Long, String> calendars = enumCalendars();
+
+        for (Map.Entry<Long, String> calendar : calendars.entrySet())
+        {
+            names.add(calendar.getValue());
+            values.add(calendar.getKey().toString());
+
+        }
+        _listPreference.setEntries((CharSequence[]) names.toArray(new CharSequence[names.size()]));
+        _listPreference.setEntryValues((CharSequence[]) values.toArray(new CharSequence[values.size()]));
+        if(names.size() > 0)
+            _listPreference.setDefaultValue(names.get(0));
+    }
+
+
+
+    /**
+     *
+     */
+    private Map<Long, String> enumCalendars() {
+        Log.d(this.LOG_TAG, "enumCalendars");
+
+        Map<Long, String> calendars = new HashMap<Long, String>();
+
+        // The indices for the projection array above.
+        final int PROJECTION_ID_INDEX = 0;
+        final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+        final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+        final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+
+        String[] projection = new String[] {
+                CalendarContract.Calendars._ID,
+                CalendarContract.Calendars.ACCOUNT_NAME,
+                CalendarContract.Calendars.CALENDAR_DISPLAY_NAME
+        };
+
+        Cursor cursor = null;
+        Uri uri = CalendarContract.Calendars.CONTENT_URI;
+
+        if(mContext == null) {
+            Log.d(this.LOG_TAG, "enumCalendars: no context! unable to get ContentResolver");
+        }
+        else {
+            cursor = mContext.getContentResolver().query(
+                    uri,
+                    projection,
+                    null,
+                    null,
+                    null);
+            while(cursor.moveToNext()) {
+
+                long id = cursor.getLong(PROJECTION_ID_INDEX);
+                String name = cursor.getString(PROJECTION_DISPLAY_NAME_INDEX);
+                String account = cursor.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+
+                calendars.put(id, name + " (" + account + ")");
+
+            }
+        }
+        return calendars;
+    }
+}
